5749c8168797a738408b07e21d5109dbcf35a9a3
Ajayrahul P <ajayrahul97@gmail.com>
2017-03-01 01:43:25 +0530

    Adds buttonnames in config

--- config.js
+++ config.js
@@ -2,31 +2,41 @@ var config={
 	
 
 //the global variables defines here
-var BoxRadius = 6,
-var InputSize = (BoxRadius*2+1)*(BoxRadius*2+1);
-var Inputs = InputSize+1;
-var  = 6; // Buttonnames  ... check this 
-var Population =300;
-var DeltaDisjoint =2.0;
-var DeltaWeights =0.4;
-var DeltaThreshold =1.0;
-var StaleSpecies=15;
-var MutateConnectionsChance =0.25;
-var PerturbChance = 0.90;
-var CrossoverChance = 0.75;
-var LinkMutationChance = 2.0;
-var NodeMutationChance = 0.50;
-var BiasMutationChance = 0.40;
-var StepSize = 0.1;
-var DisableMutationChance = 0.4;
-var EnableMutationChance = 0.2;
-var TimeoutConstant = 20;
-var MaxNodes = 1000000;
+BoxRadius :6,
+InputSize :(BoxRadius*2+1)*(BoxRadius*2+1),
+
+Inputs :InputSize+1,
+Outputs :5,
+
+ButtonNames : [
+		"A",
+		"Up",
+		"Down",
+		"Left",
+		"Right",
+	]
+
+Population :300,
+DeltaDisjoint :2.0,
+DeltaWeights :0.4,
+DeltaThreshold :1.0,
+StaleSpecies :15,
+MutateConnectionsChance :0.25,
+PerturbChance :0.90,
+CrossoverChance :0.75,
+LinkMutationChance :2.0,
+NodeMutationChance :0.50,
+BiasMutationChance :0.40,
+StepSize :0.1,
+DisableMutationChance :0.4,
+EnableMutationChance :0.2,
+TimeoutConstant :20,
+MaxNodes :1000000
 	
 };
 
-function sigmoid(x){
-	return 2/(1+math.exp(-4.9*x))-1;
+config.sigmoid = function(x){
+	return 2/(1+math.exp(-4.9*x))-1
 }
 
 module.exports = config;
